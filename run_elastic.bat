f3e152cbc2495d01e932d9300e7d6e061b1a66e2
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-09-21 11:12:55 +0300

    Create run_elastic.bat

---
+++ run_elastic.bat
@@ -0,0 +1,3 @@
+@echo off
+echo ElasticSearch starting...
+docker run --rm --name elasticsearch_container -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" elasticsearch:8.8.0
